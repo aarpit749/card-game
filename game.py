733c5d8f23f56c65c849f6c525f5e5f07c42c2eb
aarpit749 <64368066+aarpit749@users.noreply.github.com>
2020-08-23 11:17:33 +0530

    Update game.py

--- game.py
+++ game.py
@@ -17,12 +17,7 @@
 #
 # If the cards are the same rank, it is War. Each player turns up three cards face
 # down and one card face up. The player with the higher cards takes both piles
-# (six cards). If the turned-up cards are again the same rank, each player places
-# another card face down and turns another card face up. The player with the
-# higher card takes all 10 cards, and so on.
-#
-# There are some more variations on this but we will keep it simple for now.
-# Ignore "double" wars
+# (six cards). 
 #
 # https://en.wikipedia.org/wiki/War_(card_game)
 
